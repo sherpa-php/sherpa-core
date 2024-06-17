614e1d49e0c27eb908376ec10499ecb2c0ea8f09
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-06-17 23:25:48 +0200

    feat: add `httpMethod` and `path` attributes on `Route` entity class

--- src/router/Route.php
+++ src/router/Route.php
@@ -5,4 +5,29 @@ namespace Sherpa\Core\router;
 class Route
 {
 
+    private HttpMethod $httpMethod;
+    private string $path;
+
+    public function __construct($httpMethod, $path)
+    {
+        $this->httpMethod = $httpMethod;
+        $this->path = $path;
+    }
+
+    /**
+     * @return HttpMethod Route HTTP method
+     */
+    public function getHttpMethod(): HttpMethod
+    {
+        return $this->httpMethod;
+    }
+
+    /**
+     * @return string Route URI path
+     */
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
 }
