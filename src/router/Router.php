ce74f951c3aaa495c3fed9853bf865c96b7abbec
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-06-17 22:26:44 +0200

    feat: create `Router` utility class

---
+++ src/router/Router.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Sherpa\Core\router;
+
+class Router
+{
+
+    private static array $routes = [];
+
+    /**
+     * Declare a new route into router internal repository.
+     *
+     * @param HttpMethod $method HTTP request method
+     * @param string $path Web path (from URI)
+     * @return Route
+     */
+    public static function register(HttpMethod $method, string $path): Route
+    {
+        return self::$routes[] = new Route();
+    }
+
+    /**
+     * Declare a GET route into router internal repository.
+     *
+     * @param string $path Web path (from URI)
+     * @return Route
+     */
+    public static function get(string $path): Route
+    {
+        return self::register(HttpMethod::GET, $path);
+    }
+
+    /**
+     * Declare a POST route into router internal repository.
+     *
+     * @param string $path Web path (from URI)
+     * @return Route
+     */
+    public static function post(string $path): Route
+    {
+        return self::register(HttpMethod::POST, $path);
+    }
+
+}
