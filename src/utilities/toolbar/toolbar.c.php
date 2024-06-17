695ef254308df5166f3a199f0903fc4aa4465af6
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-06-18 00:52:02 +0200

    feat: add some CSS styles for Sherpa dev toolbar component

--- src/utilities/toolbar/toolbar.c.php
+++ src/utilities/toolbar/toolbar.c.php
@@ -8,14 +8,28 @@
 <style>
 
 #sherpa_dev_toolbar {
+  --background: #2d2d2d;
+
+
   position: fixed;
   bottom: 0;
   left: 0;
   right: 0;
 
-  height: 80px;
+  min-height: 60px;
 
-  background: #000;
+  background: var(--background);
+
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  column-gap: 20px;
+  row-gap: 5px;
+
+  color: white;
+  font-family: monospace;
+
+  padding: 0 50px;
 }
 
 </style>
