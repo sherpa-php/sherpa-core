b8d017b8922695b13c347dfd60949c065e784477
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-06-18 00:42:08 +0200

    feat: create `Toolbar` component file

---
+++ src/utilities/toolbar/toolbar.c.php
@@ -0,0 +1,21 @@
+<div id="sherpa_dev_toolbar" sherpa-utilities>
+
+
+
+</div>
+
+
+<style>
+
+#sherpa_dev_toolbar {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  height: 80px;
+
+  background: #000;
+}
+
+</style>
