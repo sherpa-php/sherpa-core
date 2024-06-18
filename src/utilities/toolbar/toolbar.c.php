6c1c3b5ac66310a345ce6650156082c8ef0ae724
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-06-18 03:05:52 +0200

    fix: replace `preg_replace` function by `str_replace`
    for backslashes escaping on toolbar

--- src/utilities/toolbar/toolbar.c.php
+++ src/utilities/toolbar/toolbar.c.php
@@ -45,7 +45,7 @@
   });
 
 
-  let controllerClassPathParts = "<?= preg_replace('\\', '\\\\', $currentRoute->getControllerClass()) ?>".split('\\');
+  let controllerClassPathParts = "<?= str_replace('\\', '\\\\', $currentRoute->getControllerClass()) ?>".split('\\');
 
   document.querySelector("#sherpa_dev_toolbar__route_controller > span")
       .innerText = controllerClassPathParts[controllerClassPathParts.length - 1];
