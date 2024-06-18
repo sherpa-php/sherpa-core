6f2625424089c027527b0ca9dbc7c2eb5fea2595
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-06-19 01:53:24 +0200

    feat: implement `Request` class on `Controller` class

--- src/controllers/Controller.php
+++ src/controllers/Controller.php
@@ -5,4 +5,16 @@ namespace Sherpa\Core\controllers;
 class Controller
 {
 
+    private Request $request;
+
+    public function __construct()
+    {
+        $this->request = new Request();
+    }
+
+    public function getRequest(): Request
+    {
+        return $this->request;
+    }
+
 }
