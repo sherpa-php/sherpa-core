ab5eb77f22da3c6ff70e32f65f7ce02d21e4eb3b
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-06-23 18:26:55 +0200

    feat: create `Debug` utility core class with `dump` and `dd` methods

---
+++ src/debugging/Debug.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Sherpa\Core\debugging;
+
+class Debug
+{
+
+    /**
+     * Dump given values.
+     *
+     * @param mixed ...$values
+     */
+    public static function dump(mixed ...$values): void
+    {
+        echo "<pre>";
+        var_dump(...$values);
+        echo "</pre>";
+    }
+
+    /**
+     * Dump given values and die.
+     * "Dump and Die"
+     *
+     * @param mixed ...$values
+     */
+    public static function dd(mixed ...$values): void
+    {
+        self::dump(...$values);
+        die;
+    }
+
+}
