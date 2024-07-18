ea0cfc12471020d90d4c0153aae66a66373639c1
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-07-18 18:50:59 +0200

    feat: implement Sherpa exception rendering process

--- src/exceptions/SherpaException.php
+++ src/exceptions/SherpaException.php
@@ -3,8 +3,17 @@
 namespace Sherpa\Core\exceptions;
 
 use Exception;
+use Sherpa\Core\debugging\Debug;
 
 class SherpaException extends Exception
 {
+    public function __construct()
+    {
+        $this->render();
+    }
 
+    private function render(): void
+    {
+        Debug::error($this);
+    }
 }
