bdfb204b28d45a96f91a98f7f156f7016ca9b7fa
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-07-19 20:52:58 +0200

    feat: add `isSherpaException()` method

--- src/exceptions/SherpaException.php
+++ src/exceptions/SherpaException.php
@@ -17,8 +17,23 @@ class SherpaException extends Exception
         self::render($exception);
     }
 
+    /**
+     * Render the Sherpa internal exception UI.
+     *
+     * @param SherpaException $exception Exception to show in UI
+     */
     private static function render(SherpaException $exception): void
     {
         Debug::error($exception);
+    }
+
+    /**
+     * @param Exception $exception Given exception
+     * @return bool If given exception is inherited or an object
+     *              of SherpaException class
+     */
+    public static function isSherpaException(Exception $exception): bool
+    {
+        return is_a($exception, self::class);
     }
 }
