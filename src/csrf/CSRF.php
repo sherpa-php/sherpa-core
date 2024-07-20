13af5339892abadb08f2f9a93b265682bf0b61e7
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-07-20 22:57:13 +0200

    feat: create CSRF class

---
+++ src/csrf/CSRF.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Sherpa\Core\csrf;
+
+use Sherpa\Core\utilities\generators\uuid\exceptions\InvalidUUIDException;
+use Sherpa\Core\utilities\generators\uuid\UUID;
+
+class CSRF
+{
+    private UUID $token;
+
+    public function __construct()
+    {
+        $this->generate();
+        $this->storeToken();
+    }
+
+    /**
+     * Generate a new CSRF object.
+     *
+     * @throws InvalidUUIDException
+     */
+    private function generateToken(): void
+    {
+        $this->token = UUID::generate();
+    }
+
+    /**
+     * Store as $_SESSION variable the CSRF token.
+     */
+    private function storeToken(): void
+    {
+        $_SESSION["sherpa-csrf-token"] = $this->getToken();
+    }
+
+    public function verify(string $token): bool
+    {
+        return $token === $this->getToken();
+    }
+
+    /**
+     * @return UUID CSRF token
+     */
+    public function getToken(): UUID
+    {
+        return $this->token;
+    }
+
+    public static function generate(): CSRF
+    {
+        return new self();
+    }
+}
