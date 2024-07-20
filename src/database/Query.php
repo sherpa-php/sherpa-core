55a8899ee58333684004e4d006ded6362bc8fbf1
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-07-20 21:40:57 +0200

    feat: create query execution process and getters, `get()` and `getAll()`

--- src/database/Query.php
+++ src/database/Query.php
@@ -2,16 +2,23 @@
 
 namespace Sherpa\Core\database;
 
+use PDOStatement;
+
 class Query
 {
 
     private string $query;
     private array $arguments;
 
+    private false|PDOStatement $statement;
+    private bool $executeState;
+
     public function __construct(string $query, array $arguments)
     {
         $this->query = $query;
         $this->arguments = $arguments;
+
+        $this->execute();
     }
 
     /**
@@ -28,6 +35,48 @@ class Query
     public function getArguments(): array
     {
         return $this->arguments;
+    }
+
+    /**
+     * @return false|PDOStatement Query PDO statement
+     */
+    public function getStatement()
+    {
+        return $this->statement;
+    }
+
+    /**
+     * @return int Query execution state
+     */
+    public function getExecuteState(): int
+    {
+        return $this->executeState;
+    }
+
+    private function execute(): void
+    {
+        $database = DB::getDatabase();
+
+        $this->statement = $database
+            ->prepare($this->getQuery());
+
+        $this->executeState = $this
+            ->getStatement()
+            ->execute($this->getArguments());
+    }
+
+    public function get(): mixed
+    {
+        return $this
+            ->getStatement()
+            ->fetch();
+    }
+
+    public function getAll(): array
+    {
+        return $this
+            ->getStatement()
+            ->fetchAll();
     }
 
 }
