1142025b874490aa0e3448e8e6e2519b2a30dd5a
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-07-20 22:00:21 +0200

    docs: create class methods PHPdocs

--- src/database/DB.php
+++ src/database/DB.php
@@ -7,12 +7,29 @@ use PDOException;
 
 class DB
 {
-
+    /**
+     * Create a database query and execute it.
+     *
+     * @param string $query SQL query
+     * @param mixed ...$arguments SQL query arguments array
+     * @return Query Query object
+     */
     public static function query(string $query, mixed ...$arguments): Query
     {
         return new Query($query, $arguments);
     }
 
+    /**
+     * Create a PDO object with given credentials.
+     *
+     * @param string $dbms
+     * @param string $host
+     * @param int $port
+     * @param string $db
+     * @param string $user
+     * @param string $password
+     * @return PDO
+     */
     public static function connect(
         string $dbms,
         string $host,
@@ -26,6 +43,9 @@ class DB
 
     }
 
+    /**
+     * @return PDO PDO object using current $_ENV database credentials
+     */
     public static function getDatabase(): PDO
     {
         return self::connect($_ENV["DB_DBMS"],
