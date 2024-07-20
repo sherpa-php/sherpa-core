df02058d4be144018d5f99bfea3b8527c218a876
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-07-20 23:16:32 +0200

    feat: create middleware register class

---
+++ src/middlewares/MiddlewareRegister.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Sherpa\Core\middlewares;
+
+class MiddlewareRegister
+{
+
+    private array $middlewares;
+    private static self $instance;
+
+    public function __construct()
+    {
+        $this->middlewares = [];
+    }
+
+    /**
+     * Register given middlewares.
+     *
+     * @param array $middlewares Middlewares array to register
+     */
+    public function register(array $middlewares): void
+    {
+        $this->middlewares = $middlewares;
+    }
+
+    /**
+     * @return array Registered middlewares array
+     */
+    public function getMiddlewares(): array
+    {
+        return $this->middlewares;
+    }
+
+    /**
+     * @return self Current instance
+     */
+    public static function getInstance(): self
+    {
+        if (isset(self::$instance))
+        {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+}
