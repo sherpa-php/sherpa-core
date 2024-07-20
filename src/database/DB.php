9d83f3245676e0ff975c1f5caede78ab23080e83
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-07-20 21:40:24 +0200

    chore: move database connection by `.env` values in core

--- src/database/DB.php
+++ src/database/DB.php
@@ -26,4 +26,14 @@ class DB
 
     }
 
+    public static function getDatabase(): PDO
+    {
+        self::connect($_ENV["DB_DBMS"],
+                      $_ENV["DB_HOST"],
+                      $_ENV["DB_PORT"],
+                      $_ENV["DB_DATABASE"],
+                      $_ENV["DB_USER"],
+                      $_ENV["DB_PASSWORD"]);
+    }
+
 }
