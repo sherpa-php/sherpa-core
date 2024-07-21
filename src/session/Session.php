027daf68bb4b4e79a88a97e127793dd8134c0d94
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-07-21 15:21:07 +0200

    fix: `?CSRF` as `Session::getCSRFToken()` return type

--- src/session/Session.php
+++ src/session/Session.php
@@ -7,7 +7,7 @@ use Sherpa\Core\utilities\generators\uuid\UUID;
 
 class Session
 {
-    public static function getCSRFToken(): ?UUID
+    public static function getCSRFToken(): ?CSRF
     {
         return $_SESSION["sherpa-csrf-token"] ?? null;
     }
